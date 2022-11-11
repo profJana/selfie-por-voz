2a2232ddfa6daa51f8a61bb6a4e8d27be25606af
Lucasdardot <105593880+Lucasdardot@users.noreply.github.com>
2022-11-11 14:36:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //armazenando a API na variável
 var SpeechRecognition = window.webkitSpeechRecognition;
-  
+
 //criando um novo reconhecimento de fala utilizando a API e armazenando na variável
 var recognition = new SpeechRecognition();
 
@@ -10,12 +10,12 @@ var Textbox = document.getElementById("textbox");
 function iniciar()
 {
     //sempre que o start for pressionado, a area de texto será limpa
-    Textbox.innerHTML = ""; 
+    
 
     //a função start aqui, é predefinida da API e irá iniciar a conversão de fala para texto
     recognition.start();
 } 
- 
+
 //onresult contém todos os valores da fala convertidos em texto
 recognition.onresult = function(event) {
 
@@ -27,5 +27,46 @@ recognition.onresult = function(event) {
     Textbox.innerHTML = Content;
     console.log(Content);
 
-      
+    if(Content == "selfie"){
+        console.log("tirando selfie")
+        falar()
+
+    }
+
+
+}
+
+function falar(){
+    var synth = window.speechSynthesis
+    var speakdata = "tirando selfie em 3 segundos"
+    var utterThis = new SpeechSynthesisUtterance(speakdata)
+    synth.falar(utterThis)
+    Webcam.attach(camera)
+    setTimeout(function()
+    {
+        tirarselfie()
+        salvar()
+
+    },3000)
+}
+
+camera = document.getElementById("webcam")
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:"jpeg",
+    jpeg_quality:90
+})
+
+function tirarselfie(){
+
+    Webcam.snap(function(data_uri) { document.getElementById("resultSelfie").innerHTML = '<img id="selfieImage" src="'+data_uri+'"/>'; });
+
+}
+
+function salvar(){
+    var link = document.getElementById("downloadSelfie")
+    var imagem = document.getElementById("selfieImage").scr 
+    link.href = imagem
+    link.click()
 }
